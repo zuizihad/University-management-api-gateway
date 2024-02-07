d815d27efaf341b06d5515668431a10c40f57932
Zihadul Islam <ahmedxihad@gmail.com>
2024-02-07 20:08:35 +0600

    user faculty admin created

--- src/app/modules/user/user.validations.ts
+++ src/app/modules/user/user.validations.ts
@@ -133,7 +133,6 @@ const createUserFacultyZodSchema = z.object({
       designation: z.string({
         required_error: 'Designation is required'
       }),
-      profileImage: z.string().optional()
     })
   })
 });
@@ -194,14 +193,28 @@ const createAdminZodSchema = z.object({
       designation: z.string({
         required_error: 'Designation is required'
       }),
-
-      profileImage: z.string().optional()
     })
+  })
+});
+
+const updateUser = z.object({
+  body: z.object({
+    password: z.string().optional(),
+    firstName: z.string().optional(),
+    lastName: z.string().optional(),
+    dateOfBirth: z.string().optional(),
+    gender: z.string().optional(),
+    email: z.string().email().optional(),
+    contactNo: z.string().optional(),
+    emergencyContactNo: z.string().optional(),
+    presentAddress: z.string().optional(),
+    permanentAddress: z.string().optional()
   })
 });
 
 export const UserValidation = {
   createStudentZodSchema,
   createUserFacultyZodSchema,
-  createAdminZodSchema
+  createAdminZodSchema,
+  updateUser
 };
